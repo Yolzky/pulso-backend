3b25e43c2f2e0339abe69ff24bdadf3349a3267a
hretes <hretes@sintad.pe>
2024-11-06 18:32:22 -0500

    feat: commit inicial - inicio del proyecto

---
+++ src/main/java/com/pe/pulso/PulsoApplication.java
@@ -0,0 +1,13 @@
+package com.pe.pulso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PulsoApplication.class, args);
+	}
+
+}
