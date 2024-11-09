e81bf2c3c3920dbb2027afcff597a4deb2f1ff6a
hretes <69706528+Yolzky@users.noreply.github.com>
2024-11-09 18:22:46 -0500

    feat: agregar consultas de medico especialidad por idEspecialidad y idMedico

--- src/main/java/com/pe/pulso/domain/repository/MedicoEspecialidadRepository.java
+++ src/main/java/com/pe/pulso/domain/repository/MedicoEspecialidadRepository.java
@@ -2,8 +2,15 @@ package com.pe.pulso.domain.repository;
 
 import com.pe.pulso.domain.model.entity.MedicoEspecialidad;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MedicoEspecialidadRepository extends JpaRepository<MedicoEspecialidad, Integer> {
+    @Query("SELECT me FROM MedicoEspecialidad me WHERE me.medico.idMedico = :medicoId")
+    MedicoEspecialidad findAllByMedicoId(@Param("medicoId") Integer medicoId);
+
+    @Query("SELECT me FROM MedicoEspecialidad me WHERE me.especialidad.idEspecialidad = :especialidadId")
+    MedicoEspecialidad findAllByEspecialidadId(@Param("especialidadId") Integer especialidadId);
 }
