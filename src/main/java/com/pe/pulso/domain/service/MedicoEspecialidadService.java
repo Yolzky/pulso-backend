570a93cf584a3258c3d83feb3c06300169219281
hretes <69706528+Yolzky@users.noreply.github.com>
2024-11-09 18:23:47 -0500

    feat: agregar fiurmas de Service de MedicoEspecialidad find by medico and especialidad Id

--- src/main/java/com/pe/pulso/domain/service/MedicoEspecialidadService.java
+++ src/main/java/com/pe/pulso/domain/service/MedicoEspecialidadService.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Service;
 
 @Service
 public interface MedicoEspecialidadService extends GenericService<MedicoEspecialidad, SaveMedicoEspecialidadDto, Integer>{
+    MedicoEspecialidad findAllByMedicoId(Integer medicoId);
+    MedicoEspecialidad findAllByEspecialidadId(Integer especialidadId);
 }
